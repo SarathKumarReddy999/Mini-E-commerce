b4f5e36f3590266ce47538532e08be470620d3c3
sarathkumar999 <sarathkumar170901@gmail.com>
2026-06-06 16:01:15 +0530

    add api to fetch prodect by id

--- product/src/main/java/com/ecommerce/product/controller/ProductController.java
+++ product/src/main/java/com/ecommerce/product/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.ecommerce.product.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +33,11 @@ public class ProductController {
     @PostMapping
     public Product createProduct(@RequestBody Product product) {
         return repository.save(product);
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found!"));
     }
 }
